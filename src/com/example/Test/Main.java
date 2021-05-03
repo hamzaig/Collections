49684282076a613df248a5b5c08b5a538b8c7e5c
hamzaig <hamzaig@yahoo.com>
2021-05-03 15:11:44 +0500

    Initial commit

---
+++ src/com/example/Test/Main.java
@@ -0,0 +1,9 @@
+package com.example.Test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Super",8,12);
+        theatre.getSeats();
+    }
+}
